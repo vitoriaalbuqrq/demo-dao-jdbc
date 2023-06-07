a14cdf853248bf79ddb9f2a1fd5621fc12d24312
Vitoria Albuquerque <albuquerquevitoria14@gmail.com>
2023-06-07 16:29:29 -0300

    DepartmentDaoJDBC and department findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 /*a classe vai expor um metodo que retorna o 
@@ -10,5 +11,9 @@ public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
